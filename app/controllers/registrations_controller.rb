21a5ef8ac7e9e2540f147bd9080f891c9e22c032
Liz Bayardelle <lizbayardelle@gmail.com>
2019-08-11 20:07:13 -0700

    User Creation with Preauthorization

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
   # prepend_before_action :check_captcha, only: :create
   invisible_captcha only: [:create], honeypot: :subtitle
+  after_action :update_authorizations, only: [:create]
 
   def create
     super
@@ -23,6 +24,20 @@ class RegistrationsController < Devise::RegistrationsController
   # def after_sign_up_path_for(resource)
   #   static_path(:ThankYou)
   # end
+
+  def update_authorizations
+    authorizations = Authorization.all
+
+    authorizations.each do |a|
+      if @user.email == a.email
+        a.account_created = true
+        a.save
+        @user.admin = a.admin
+        @user.committee = a.committee
+        @user.save
+      end
+    end
+  end
 
 
   def sign_up_params
