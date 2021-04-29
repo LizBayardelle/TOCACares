55e4f53745cd5f8edca6ad169c3ff23ce6ab56b2
Liz Bayardelle <lizbayardelle@gmail.com>
2021-04-29 14:59:41 -0700

    hotfix

--- app/models/variation.rb
+++ app/models/variation.rb
@@ -1,5 +1,5 @@
 class Variation < ApplicationRecord
-  belongs_to :user
-  belongs_to :app_form
-  belongs_to :original_app_form, class_name: "AppForm"
+  # belongs_to :user
+  # belongs_to :app_form
+  # belongs_to :original_app_form, class_name: "AppForm"
 end
